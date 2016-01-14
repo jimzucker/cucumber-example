4ba84d6d7bf5d5db318852e21f352037c11947c9
Jim Zucker <jim@stratengllc.com>
2016-01-13 16:56:13 -0800

    first commit

---
+++ src/test/java/com/stratengllc/bdd/runner/RunCalculatorTest.java
@@ -0,0 +1,15 @@
+package com.stratengllc.bdd.runner;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(
+        format = { "pretty", "html:target/cucumber" },
+        glue = "com.stratengllc.bdd.steps",
+        features = "classpath:cucumber/calculator.feature"
+)
+public class RunCalculatorTest {
+}
