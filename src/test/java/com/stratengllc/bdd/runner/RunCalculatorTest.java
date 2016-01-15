873b1d2155edb839ddc924dab0516143cce72c71
Jim Zucker <jim@stratengllc.com>
2016-01-14 20:30:27 -0800

    create cucumber.json

--- src/test/java/com/stratengllc/bdd/runner/RunCalculatorTest.java
+++ src/test/java/com/stratengllc/bdd/runner/RunCalculatorTest.java
@@ -5,9 +5,8 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-
 @CucumberOptions(
-        format = { "pretty", "html:target/cucumber" },
+	format = {"pretty", "html:target/cucumber-htmlreport","json:target/cucumber-report.json"},
         glue = "com.stratengllc.bdd.steps",
         features = "classpath:cucumber/calculator.feature"
 )
